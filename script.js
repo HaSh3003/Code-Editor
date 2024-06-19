ba1a77b99414cb6b397ef52137984488258c9656
HeshAmYasSeR | Perfecto <64496882+HaSh3003@users.noreply.github.com>
2024-06-19 05:53:32 +0200

    update With Code Mirror

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
 function run() {
-  let htmlCode = document.getElementById("Html-code").value;
-  let cssCode = document.getElementById("Css-code").value;
-  let jsCode = document.getElementById("Js-code").value;
+  let htmlCode = HtmlEditor.getValue();
+  let cssCode = CssEditor.getValue();
+  let jsCode = JsEditor.getValue();
   let output = document.getElementById("output");
+
 
   output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
   output.contentWindow.eval(jsCode);
 
 
 }
+
+let HtmlEditor = CodeMirror.fromTextArea(document.getElementById("Html-code"), {
+  name: "htmlmixed",
+  theme: "ayu-dark",
+  lineWrapping: true,
+  lineNumbers: true,
+  extraKeys: { "Ctrl-Space": "autocomplete" },
+  hintOptions: { schemaInfo: CodeMirror.htmlSchema }
+});
+
+
+let CssEditor = CodeMirror.fromTextArea(document.getElementById("Css-code"), {
+  mode: "css",
+  theme: "ayu-dark",
+  lineNumbers: true,
+  lineWrapping: true,
+  extraKeys: { "Ctrl-Space": "autocomplete" },
+  hintOptions: { schemaInfo: CodeMirror.cssSchema }
+});
+
+
+let JsEditor = CodeMirror.fromTextArea(document.getElementById("Js-code"), {
+  mode: "javascript",
+  theme: 'ayu-dark',
+  lineWrapping: true,
+  lineNumbers: true,
+  hintOptions: { schemaInfo: CodeMirror.javascriptSchema }
+});
+
+
+HtmlEditor.on('inputRead', function(cm, event) {
+  if (!cm.state.completionActive && event.text.length === 1) {
+    cm.showHint({ completeSingle: false });
+  }
+});
+
+
+CssEditor.on('inputRead', function(cm, event) {
+  if (!cm.state.completionActive && event.text.length === 1) {
+    cm.showHint({ completeSingle: false });
+  }
+});
+
+
+JsEditor.on('inputRead', function(cm, event) {
+  if (!cm.state.completionActive && event.text.length === 1) {
+    cm.showHint({ completeSingle: false });
+  }
+});
+HtmlEditor.on('keyup', function(cm, event) {
+  run();
+});
+
+CssEditor.on('keyup', function(cm, event) {
+  run();
+});
+
+JsEditor.on('keyup', function(cm, event) {
+  run();
+});
