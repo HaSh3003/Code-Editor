8dd9cb41661373964fefbabd7609edfcc82d6ba2
HeshAmYasSeR | Perfecto <64496882+HaSh3003@users.noreply.github.com>
2024-06-21 08:02:54 +0200

    Resize Inputs Added

--- script.js
+++ script.js
@@ -4,22 +4,18 @@ function run() {
   let jsCode = JsEditor.getValue();
   let output = document.getElementById("output");
 
-
   output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
   output.contentWindow.eval(jsCode);
-
-
 }
 
 let HtmlEditor = CodeMirror.fromTextArea(document.getElementById("Html-code"), {
-  name: "htmlmixed",
+  mode: "htmlmixed",
   theme: "ayu-dark",
   lineWrapping: true,
   lineNumbers: true,
   extraKeys: { "Ctrl-Space": "autocomplete" },
   hintOptions: { schemaInfo: CodeMirror.htmlSchema }
 });
-
 
 let CssEditor = CodeMirror.fromTextArea(document.getElementById("Css-code"), {
   mode: "css",
@@ -30,35 +26,32 @@ let CssEditor = CodeMirror.fromTextArea(document.getElementById("Css-code"), {
   hintOptions: { schemaInfo: CodeMirror.cssSchema }
 });
 
-
 let JsEditor = CodeMirror.fromTextArea(document.getElementById("Js-code"), {
   mode: "javascript",
-  theme: 'ayu-dark',
+  theme: "ayu-dark",
   lineWrapping: true,
   lineNumbers: true,
   hintOptions: { schemaInfo: CodeMirror.javascriptSchema }
 });
 
-
 HtmlEditor.on('inputRead', function(cm, event) {
   if (!cm.state.completionActive && event.text.length === 1) {
-    cm.showHint({ completeSingle: false });
+      cm.showHint({ completeSingle: false });
   }
 });
-
 
 CssEditor.on('inputRead', function(cm, event) {
   if (!cm.state.completionActive && event.text.length === 1) {
-    cm.showHint({ completeSingle: false });
+      cm.showHint({ completeSingle: false });
   }
 });
-
 
 JsEditor.on('inputRead', function(cm, event) {
   if (!cm.state.completionActive && event.text.length === 1) {
-    cm.showHint({ completeSingle: false });
+      cm.showHint({ completeSingle: false });
   }
 });
+
 HtmlEditor.on('keyup', function(cm, event) {
   run();
 });
@@ -70,3 +63,30 @@ CssEditor.on('keyup', function(cm, event) {
 JsEditor.on('keyup', function(cm, event) {
   run();
 });
+
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+let iframe = document.getElementById("output");
+
+function Devices_Emulator() {
+  iframe.style.width = width.value + "px";
+  iframe.style.height = height.value + "px";
+}
+
+width.addEventListener('keyup', function() {
+  Devices_Emulator();
+});
+
+height.addEventListener('keyup', function() {
+  Devices_Emulator();
+});
+
+function updateValues() {
+  width.value = iframe.offsetWidth;
+  height.value = iframe.offsetHeight;
+}
+
+const resizeObserver = new ResizeObserver(updateValues);
+resizeObserver.observe(iframe);
+
+updateValues();
